5a21ee5a9a16f370ee30bd6fc75e6971781a5497
Brian O'Reilly <fade@deepsky.com>
2023-01-04 12:50:41 -0500

    actually make some of the configuration appear in the repository.

---
+++ custom.el
@@ -0,0 +1,223 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-faces-vector
+   [default bold shadow italic underline success warning error])
+ '(ansi-color-names-vector
+   ["#000000" "#8b0000" "#00ff00" "#ffa500" "#7b68ee" "#dc8cc3" "#93e0e3" "#dcdccc"])
+ '(awesome-tray-mode-line-active-color "#2fafff")
+ '(awesome-tray-mode-line-inactive-color "#323232")
+ '(column-number-mode t)
+ '(compilation-message-face 'default)
+ '(cua-global-mark-cursor-color "#2aa198")
+ '(cua-normal-cursor-color "#839496")
+ '(cua-overwrite-cursor-color "#b58900")
+ '(cua-read-only-cursor-color "#859900")
+ '(custom-enabled-themes '(deepsky))
+ '(custom-safe-themes
+   '("7212337b36e4cce2628c9451173d910c728b5a760a0ce17cf82cd41af907bd9b" "7e22a8dcf2adcd8b330eab2ed6023fa20ba3b17704d4b186fa9c53f1fab3d4d2" "893e6ba11e50a0f2007d0fe67e0d16e9c04aa016ecfd9892c8ed3233d98cecd3" "210e1f1c1b675e33aa400075c5c41f26440b2f0dcbe8ad7368a71b92012c267e" "3aed30c256f3506b39fdf4a7143168f9ca624947a7ea77c08cf7bd65c0dab9b7" "f9b8de70122e2370c69f59592cfc162f55ceded36cbbf7f8962f4985124e949f" "eb6815efcf4b3506804a1aca00b4100bdf7a76664c6396c77a760791fd1fa6cb" "0becd5fdab50c67580f8ab05c71d8c632a24e5ede5f657ebf71d5f9fca3b879c" "e6cace7f97c0c42dd9656a8db27c92f27b6be6b2be3a44ed9f304648cf13a153" "dd8f89c0930a74fec169fa779822376a826147db9ecf784a2a12a7ae4d0ba2b5" "f1db6b75f93a964718258bf1cb78993b6b654468b1ee1e26df74c44ba99e8c4e" "cc8f5f7bdd13efd250c01db39a5193c948fba7c02e5ddc478f60d08347fe5405" "b825687675ea2644d1c017f246077cdd725d4326a1c11d84871308573d019f67" "235dc2dd925f492667232ead701c450d5c6fce978d5676e54ef9ca6dd37f6ceb" "4f2ede02b3324c2f788f4e0bad77f7ebc1874eff7971d2a2c9b9724a50fb3f65" "f0a99f53cbf7b004ba0c1760aa14fd70f2eabafe4e62a2b3cf5cabae8203113b" default))
+ '(display-time-mode t)
+ '(fci-rule-color "#383838")
+ '(flymake-error-bitmap '(flymake-double-exclamation-mark modus-theme-fringe-red))
+ '(flymake-note-bitmap '(exclamation-mark modus-theme-fringe-cyan))
+ '(flymake-warning-bitmap '(exclamation-mark modus-theme-fringe-yellow))
+ '(fringe-mode 6 nil (fringe))
+ '(helm-ag-base-command "ag --nocolor --nogroup --ignore-case")
+ '(helm-ag-command-option "--all-text")
+ '(helm-ag-ignore-buffer-patterrns '("\\.txt\\'" "\\.mkd\\'"))
+ '(helm-ag-insert-at-point 'symbol)
+ '(highlight-changes-colors '("#d33682" "#6c71c4"))
+ '(highlight-symbol-colors
+   (--map
+    (solarized-color-blend it "#002b36" 0.25)
+    '("#b58900" "#2aa198" "#dc322f" "#6c71c4" "#859900" "#cb4b16" "#268bd2")))
+ '(highlight-symbol-foreground-color "#93a1a1")
+ '(highlight-tail-colors
+   '(("#073642" . 0)
+     ("#546E00" . 20)
+     ("#00736F" . 30)
+     ("#00629D" . 50)
+     ("#7B6000" . 60)
+     ("#8B2C02" . 70)
+     ("#93115C" . 85)
+     ("#073642" . 100)))
+ '(hl-bg-colors
+   '("#7B6000" "#8B2C02" "#990A1B" "#93115C" "#3F4D91" "#00629D" "#00736F" "#546E00"))
+ '(hl-fg-colors
+   '("#002b36" "#002b36" "#002b36" "#002b36" "#002b36" "#002b36" "#002b36" "#002b36"))
+ '(hl-todo-keyword-faces
+   '(("HOLD" . "#cfdf30")
+     ("TODO" . "#feacd0")
+     ("NEXT" . "#b6a0ff")
+     ("THEM" . "#f78fe7")
+     ("PROG" . "#00d3d0")
+     ("OKAY" . "#4ae8fc")
+     ("DONT" . "#80d200")
+     ("FAIL" . "#ff8059")
+     ("BUG" . "#ff8059")
+     ("DONE" . "#44bc44")
+     ("NOTE" . "#f0ce43")
+     ("KLUDGE" . "#eecc00")
+     ("HACK" . "#eecc00")
+     ("TEMP" . "#ffcccc")
+     ("FIXME" . "#ff9977")
+     ("XXX+" . "#f4923b")
+     ("REVIEW" . "#6ae4b9")
+     ("DEPRECATED" . "#bfd9ff")))
+ '(ibuffer-deletion-face 'modus-theme-mark-del)
+ '(ibuffer-filter-group-name-face 'modus-theme-mark-symbol)
+ '(ibuffer-marked-face 'modus-theme-mark-sel)
+ '(ibuffer-title-face 'modus-theme-pseudo-header)
+ '(jdee-db-active-breakpoint-face-colors (cons "#1E2029" "#bd93f9"))
+ '(jdee-db-requested-breakpoint-face-colors (cons "#1E2029" "#50fa7b"))
+ '(jdee-db-spec-breakpoint-face-colors (cons "#1E2029" "#565761"))
+ '(linum-format " %7d ")
+ '(magit-diff-use-overlays nil)
+ '(nrepl-message-colors
+   '("#dc322f" "#cb4b16" "#b58900" "#546E00" "#B4C342" "#00629D" "#2aa198" "#d33682" "#6c71c4"))
+ '(objed-cursor-color "#ff5555")
+ '(package-selected-packages
+   '(embark marginalia orderless consult vertico ob-restclient terraform-doc terraform-mode telephone-line 0x0 lorem-ipsum bug-hunter setup forge yasnippet avy spaceline-all-the-icons perspective dockerfile-mode docker company-quickhelp hydra orgtbl-aggregate abyss-theme sparql-mode pdf-tools extempore-mode helpful skeletor :skeletor spaceline-config modus-vivendi-theme all-the-icons-dired dired-single jedi doct visual-fill-column which-key doom-modeline doom-themes color-identifiers-mode flycheck-eslint flycheck-nim flycheck-clojure flycheck-cython flycheck-ido company-org-roam org-roam-server org-roam-bibtex org-roam ac-helm ac-sly sly-ac helm-sly :flycheck yatemplate sly-quicklisp sly-repl-ansi-color nim-mode :sly-quicklisp sly-repl-ansi-colors sly-named-readtables sly-macrostep nord-theme northcode-theme nova-theme sly-asdf all-the-icons sly buffer-move slime yasnippet-snippets helm-ispell magithub helm-slime csv csv-mode slime-helm eww-lnum symon undotree common-lisp-snippets swiper projectile diminish smart-mode-line-powerline-theme fancy-battery spaceline dashboard pretty-mode color-theme-approximate 2048-game org-bullets beacon jinja2-mode company-ansible ansible-vault ansible-doc zoom ansible htmlize hcl-mode helm-w3m :helm-w3m w3m yaml-mode paredit shell-pop scratch zoom-frm emmet-mode visual-regexp-steroids visual-regexp typescript-mode smooth-scrolling skewer-mode smartscan restclient geiser racket-mode olivetti multiple-cursors markdown-mode macrostep gist flycheck expand-region magit helm-swoop helm-ag helm-projectile helm company clojure-mode c-eldoc android-mode ace-window ace-jump-mode ag ido-vertical-mode transpose-frame ox-pandoc company-jedi js-auto-beautify js-comint js-doc js-format js-import js2-refactor loccur hexrgb purple-haze-theme heroku-theme company-anaconda elpy rainbow-mode slime-repl-ansi-color slime-company rust-mode crux undo-tree latex-preview-pane latex-extra xquery-mode virtualenvwrapper quickrun exec-path-from-shell page-break-lines solarized-theme cyberpunk-theme))
+ '(pdf-view-midnight-colors (cons "#f8f8f2" "#282a36"))
+ '(pos-tip-background-color "#073642")
+ '(pos-tip-foreground-color "#93a1a1")
+ '(rustic-ansi-faces
+   ["#282a36" "#ff5555" "#50fa7b" "#f1fa8c" "#61bfff" "#ff79c6" "#8be9fd" "#f8f8f2"])
+ '(safe-local-variable-values
+   '((Package . CL-USER)
+     (org-edit-src-content-indentation 0)
+     (Package . TRIVIAL-GRAY-STREAMS)
+     (Syntax . ANSI-Common-lisp)
+     (eval cl-flet
+           ((enhance-imenu-lisp
+             (&rest keywords)
+             (dolist
+                 (keyword keywords)
+               (add-to-list 'lisp-imenu-generic-expression
+                            (list
+                             (purecopy
+                              (concat
+                               (capitalize keyword)
+                               (if
+                                   (string=
+                                    (substring-no-properties keyword -1)
+                                    "s")
+                                   "es" "s")))
+                             (purecopy
+                              (concat "^\\s-*("
+                                      (regexp-opt
+                                       (list
+                                        (concat "define-" keyword))
+                                       t)
+                                      "\\s-+\\(" lisp-mode-symbol-regexp "\\)"))
+                             2)))))
+           (enhance-imenu-lisp "bookmarklet-command" "class" "command" "ffi-method" "function" "internal-page-command" "internal-page-command-global" "mode" "parenscript" "user-class"))
+     (Package . POSTMODERN)
+     (Package . CIRCULAR-BUFFER)
+     (encoding utf-8)
+     (Package . S-SQL)
+     (package . RFC2388)
+     (Package . FLEXI-STREAMS)
+     (Package . CHUNGA)
+     (Package . CL-PPCRE)
+     (Package . CL-POSTGRES-TRIVIAL-UTF-8)
+     (Package . CL-POSTGRES)
+     (Syntax . Ansi-Common-Lisp)
+     (Package . CL-WHO)
+     (Package . LALR)
+     (readtable . runes)
+     (Package . CXML)
+     (Syntax . Common-Lisp)
+     (package . rune-dom)
+     (Package . DRAKMA)
+     (Package . HUNCHENTOOT)
+     (Package . FSet)
+     (eval cl-flet
+           ((enhance-imenu-lisp
+             (&rest keywords)
+             (dolist
+                 (keyword keywords)
+               (add-to-list 'lisp-imenu-generic-expression
+                            (list
+                             (purecopy
+                              (concat
+                               (capitalize keyword)
+                               (if
+                                   (string=
+                                    (substring-no-properties keyword -1)
+                                    "s")
+                                   "es" "s")))
+                             (purecopy
+                              (concat "^\\s-*("
+                                      (regexp-opt
+                                       (list
+                                        (concat "define-" keyword))
+                                       t)
+                                      "\\s-+\\(" lisp-mode-symbol-regexp "\\)"))
+                             2)))))
+           (enhance-imenu-lisp "bookmarklet-command" "class" "command" "function" "mode" "parenscript" "user-class"))
+     (Package . CL-UNICODE)
+     (Package . containers)
+     (eval put 'test-js-eval 'common-lisp-indent-function 1)
+     (eval put 'test-ps-js 'common-lisp-indent-function 1)
+     (indent-tabs)
+     (Package . CL-FAD)
+     (Syntax . COMMON-LISP)
+     (Package . Lexical-Contexts)
+     (Base . 10)
+     (Syntax . ANSI-Common-Lisp)
+     (eval sh-set-shell "zsh")))
+ '(send-mail-function 'mailclient-send-it)
+ '(show-paren-mode t)
+ '(smartrep-mode-line-active-bg (solarized-color-blend "#859900" "#073642" 0.2))
+ '(spaceline-all-the-icons-separator-type 'wave)
+ '(term-default-bg-color "#002b36")
+ '(term-default-fg-color "#839496")
+ '(tool-bar-mode nil)
+ '(vc-annotate-background "#2B2B2B")
+ '(vc-annotate-background-mode nil)
+ '(vc-annotate-color-map
+   '((20 . "#BC8383")
+     (40 . "#CC9393")
+     (60 . "#DFAF8F")
+     (80 . "#D0BF8F")
+     (100 . "#E0CF9F")
+     (120 . "#F0DFAF")
+     (140 . "#5F7F5F")
+     (160 . "#7F9F7F")
+     (180 . "#8FB28F")
+     (200 . "#9FC59F")
+     (220 . "#AFD8AF")
+     (240 . "#BFEBBF")
+     (260 . "#93E0E3")
+     (280 . "#6CA0A3")
+     (300 . "#7CB8BB")
+     (320 . "#8CD0D3")
+     (340 . "#94BFF3")
+     (360 . "#DC8CC3")))
+ '(vc-annotate-very-old-color "#DC8CC3")
+ '(warning-suppress-types '((browse-url) (comp)))
+ '(weechat-color-list
+   '(unspecified "#002b36" "#073642" "#990A1B" "#dc322f" "#546E00" "#859900" "#7B6000" "#b58900" "#00629D" "#268bd2" "#93115C" "#d33682" "#00736F" "#2aa198" "#839496" "#657b83"))
+ '(xterm-color-names
+   ["#073642" "#dc322f" "#859900" "#b58900" "#268bd2" "#d33682" "#2aa198" "#eee8d5"])
+ '(xterm-color-names-bright
+   ["#002b36" "#cb4b16" "#586e75" "#657b83" "#839496" "#6c71c4" "#93a1a1" "#fdf6e3"]))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(company-scrollbar-bg ((t (:background "#199919991999"))) t)
+ '(company-scrollbar-fg ((t (:background "#0ccc0ccc0ccc"))) t)
+ '(company-tooltip ((t (:inherit default :background "#051e051e051e"))))
+ '(company-tooltip-common ((t (:inherit font-lock-constant-face))))
+ '(company-tooltip-scrollbar-thumb ((t (:background "#0ccc0ccc0ccc"))))
+ '(company-tooltip-scrollbar-track ((t (:background "#199919991999"))))
+ '(company-tooltip-selection ((t (:inherit font-lock-function-name-face))))
+ '(font-lock-constant-face ((t (:foreground "#00ff7f"))))
+ '(font-lock-string-face ((t (:foreground "cornsilk3"))))
+ '(helm-bookmark-directory ((t (:inherit helm-ff-directory))))
+ '(helm-buffer-directory ((t (:inherit helm-ff-directory)))))
---
+++ init.el
@@ -0,0 +1,176 @@
+;;; init.el --- Base emacs config file -*- lexical-binding: t; -*-
+;;; Commentary:
+;; Copyright (c) 2018-2022 Brian O'Reilly <fade@deepsky.com>
+
+;;; Code:
+
+;;; Silence compiler warnings from native-comp; they're too disruptive.
+(setq native-comp-async-report-warnings-errors nil)
+
+;;; Set up package
+(require 'package)
+
+(setq package-archives '(("melpa" . "https://melpa.org/packages/")
+                         ("ELPA"  . "http://tromey.com/elpa/")
+			 ("gnu"   . "http://elpa.gnu.org/packages/"))) ;;("org"   . "https://orgmode.org/elpa/")
+
+(package-initialize)
+
+;;; Bootstrap straight package manager
+;; Install straight.el
+(defvar bootstrap-version)
+
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 5))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+
+
+;;; Bootstrap use-package
+
+(unless (package-installed-p 'use-package)
+  (straight-use-package 'use-package))
+
+
+;; From use-package README
+
+(eval-when-compile
+  (require 'use-package))
+
+(require 'bind-key)
+(setq use-package-verbose t)
+
+;; use the straight sources for use-package by default
+;; (use-package straight
+;;   :custom (straight-use-package-by-default t))
+
+;; sometimes when I visit an org file, org-mode does not start. moving
+;; mode installation here, to test whether it is a load-time phasing
+;; issue. (it was, leave this as is.)
+
+(defun efs/org-mode-setup ()
+  (org-indent-mode)
+  (variable-pitch-mode 1)
+  (visual-line-mode 1))
+
+(use-package org
+  :hook (org-mode . efs/org-mode-setup)
+  :straight t
+  :custom
+  (org-ellipsis " ▾")
+  (org-agenda-start-with-log-mode t)
+  (org-log-done 'time)
+  (org-log-into-drawer t)
+  (org-todo-keywords
+   '((sequence "TODO(t)" "NEXT(n)" "|" "DONE(d!)")
+     (sequence "BACKLOG(b)" "PLAN(p)" "READY(r)" "ACTIVE(a)" "REVIEW(v)" "WAIT(w@/!)" "HOLD(h)" "|" "COMPLETED(c)" "CANC(k@)")))
+  :config
+  (progn (eval-after-load "org"
+           '(require 'ox-md nil t))
+         (setq org-support-shift-select 'always)
+         ;; rename org-roam buffers to their enclosed #+TITLE
+         (add-hook 'org-mode-hook 'fade/org-mode-rename-buffer)))
+
+;; I put 'org-spiffs in a subdir of emacs.d, which needs finding:
+(add-to-list 'load-path (expand-file-name "fade/" user-emacs-directory))
+(require 'org-spiffs nil t) ;; rename org-roam buffers to something sane.
+
+;;; Themes need finding.
+(add-to-list 'custom-theme-load-path
+             (expand-file-name "themes/" user-emacs-directory))
+
+;; where I'm developing my theme.
+(add-to-list 'custom-theme-load-path
+             (expand-file-name "themes/" "~/SourceCode/lisp/emacs_stuff/"))
+
+;;; Begin initialization
+;; Turn off mouse interface early in startup to avoid momentary display
+(when window-system
+  (menu-bar-mode -1)
+  (tool-bar-mode -1)
+  (scroll-bar-mode -1)
+  (tooltip-mode -1)
+  (display-time-mode 1))
+
+(set-frame-parameter nil 'fullscreen 'maximized)
+(setq inhibit-startup-message t)
+(setq initial-scratch-message "")
+
+;; Set path to local dependencies
+(setq site-lisp-dir
+      (expand-file-name "site-lisp" user-emacs-directory))
+(setq settings-dir
+      (expand-file-name "settings" user-emacs-directory))
+(setq site-org-files
+      (expand-file-name "OrgFiles" "~/Dropbox/"))
+
+
+(add-to-list 'auto-mode-alist '("\\.zsh\\'" . sh-mode))
+(add-to-list 'auto-mode-alist '("\\.zshrc\\'" . sh-mode))
+
+;; Keep emacs Custom-settings in separate file
+(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
+(load custom-file)
+
+;; Add external projects to load path
+(dolist (project (directory-files site-lisp-dir t "\\w+"))
+  (when (file-directory-p project)
+    (add-to-list 'load-path project)))
+
+
+;; Write backup files to their own directory
+(setq backup-directory-alist
+      `(("." . ,(expand-file-name
+                 (concat user-emacs-directory "backups")))))
+
+;; Make backups of files, even when they're in version control
+(setq vc-make-backup-files t)
+
+;; configure dired early, because it is built in.
+(use-package dired
+  :ensure nil
+  :commands (dired dired-jump)
+  :bind (("C-x C-j" . dired-jump))
+  :custom ((dired-listing-switches "-hal --group-directories-first")))
+
+
+;; start the emacs daemon process.
+(server-start)
+
+;;; Load the config contained in our Org-mode file, which contains the
+;;; meat of this config..
+(org-babel-load-file (concat user-emacs-directory "config.org"))
+
+;;; reinstall all installed packages, when emacs changes version
+
+(defun package-reinstall-all-activated-packages ()
+  "Refresh and reinstall all activated packages."
+  (interactive)
+  (package-refresh-contents)
+  (dolist (package-name package-activated-list)
+    (when (package-installed-p package-name)
+      (unless (ignore-errors                   ;some packages may fail to install
+                (package-reinstall package-name))
+        (warn "Package %s failed to reinstall" package-name)))))
+
+;; late setup
+(require 'sraight-spiffs nil t) ;; when I manually update straight
+;; packages, do all the steps at once.
+
+(defun show-file-name ()
+  "Show the full path filename of the current buffer in the minibuffer."
+  (interactive)
+  (message (buffer-file-name))
+  (kill-new (file-truename buffer-file-name)))
+
+(global-set-key "\C-cz" 'show-file-name)
+
+(provide 'init)
+;;; init.el ends here

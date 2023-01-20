3446372066b355aa4565c0f03e06f1f6bec31d2e
Brian O'Reilly <fade@deepsky.com>
2023-01-20 15:25:06 -0500

    fix typo

--- fade/org-spiffs.el
+++ fade/org-spiffs.el
@@ -1,7 +1,7 @@
 ;;;-*- lexical-binding: t; -*-
 ;;; init.el --- Base emacs config file
 ;;; Commentary:
-;; Copyright (c) 2018-2021 Brian O'Reilly <fade@deepsky.com>
+;;; Copyright (c) 2018-2021 Brian O'Reilly <fade@deepsky.com>
 ;;; Code:
 
 (require 'cl-lib)

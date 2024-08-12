e44d4c52de7d4db5f85b0b07fd244c4ae1d82b33
Brian O'Reilly <fade@deepsky.com>
2024-08-12 12:25:56 -0400

    29.4 still evinces this function signature deprecation.

--- init.el
+++ init.el
@@ -30,7 +30,7 @@
 ;; where we hold transient saves and other such data. The arity of
 ;; files--ensure-directory changed from emacs29 to emacs30.
 
-(if (version<= emacs-version "29.3")
+(if (version<= emacs-version "29.4")
     (progn
       (files--ensure-directory 'make-directory-internal (expand-file-name "config/" user-emacs-directory))
       (files--ensure-directory 'make-directory-internal (expand-file-name "data/" user-emacs-directory)))
